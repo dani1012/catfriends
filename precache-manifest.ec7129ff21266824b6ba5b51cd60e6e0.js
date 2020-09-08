f986ac4993732bec3c952eebb4779d3f0ea3f626
dani1012 <daniyang1012@gmail.com>
2020-09-08 18:02:27 +1000

    Updates

---
+++ precache-manifest.ec7129ff21266824b6ba5b51cd60e6e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9deb970e8376d4f4fb64358d4d02b874",
+    "url": "/catfriends/index.html"
+  },
+  {
+    "revision": "85b9e75bb2de74700b54",
+    "url": "/catfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3609ac99dda190e26c4d",
+    "url": "/catfriends/static/css/main.d436d932.chunk.css"
+  },
+  {
+    "revision": "85b9e75bb2de74700b54",
+    "url": "/catfriends/static/js/2.a0d1bd17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/catfriends/static/js/2.a0d1bd17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3609ac99dda190e26c4d",
+    "url": "/catfriends/static/js/main.c98ae99f.chunk.js"
+  },
+  {
+    "revision": "99ba04f2dfe65464d3cb",
+    "url": "/catfriends/static/js/runtime-main.095099bc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/catfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
